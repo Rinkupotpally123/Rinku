6478f2b932e62e0aa00d8c42d5ab9e79d63f1da3
rinku <rinku@192.168.0.11>
2023-08-23 20:35:27 +0530

    Its my first commit of GitHub

---
+++ src/basicseleniumprograms/Example/TestingLibraries.java
@@ -0,0 +1,14 @@
+package basicseleniumprograms.Example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestingLibraries {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+WebDriver driver=new ChromeDriver();
+driver.get("https://www.youtube.com/");
+	}
+
+}
